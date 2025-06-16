3a7b14cc4170cd09d713579d2b18bfd9cd3ebb9a
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-16 19:51:10 +0200

    peer reviewed update

--- Nivel 1/index.php
+++ Nivel 1/index.php
@@ -4,11 +4,10 @@ require_once 'cat.php';
 require_once 'dog.php';
 
 $dog1 = new Dog("Shadow");
-echo $dog1->name . " does " . $dog1->makeSound() . "\n";
-
 $cat1 = new Cat("Panteri");
-echo $cat1->name . " does " . $cat1->makeSound() . "\n";
-
 $cat2 = new Cat("Frodo");
+
+echo $dog1->name . " does " . $dog1->makeSound() . "\n";
+echo $cat1->name . " does " . $cat1->makeSound() . "\n";
 echo $cat2->name . " does " . $cat2->makeSound() . "\n";
 ?>
