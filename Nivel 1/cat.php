f921b11b156ac9d5ef49b7fc50eb06850a28a403
Asier Carrasco González <axiercarrasco@gmail.com>
2025-06-16 11:38:28 +0200

    changes after peer review

--- Nivel 1/cat.php
+++ Nivel 1/cat.php
@@ -1,11 +1,5 @@
 <?php
 class Cat extends Animal {
-    public $name;
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
     public function makeSound() {
         return "Miau, Miau";
     }
--- Nivel 1/dog.php
+++ Nivel 1/dog.php
@@ -1,11 +1,5 @@
 <?php
 class Dog extends Animal {
-    public $name;
-
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
     public function makeSound() {
         return "Woof, Woof";
     }
